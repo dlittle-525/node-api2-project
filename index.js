38805b3eb0871102cd7a3200fad8f079d68d04e5
Douglas Little <douglas.a.little@gmail.com>
2021-04-15 19:47:14 -0400

    Updates for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require your server and launch it here
 const server = require("./api/server")
+const port = process.env.PORT || 5000
 
-server.listen(5000, () => {
+server.listen(port, () => {
     console.log("Server running at http://localhost:5000")
 })
